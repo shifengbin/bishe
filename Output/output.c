d87bad36e0fc3e416a63d7be85d1c010b29ccbdd
Linux <shifengbin@ubuntu.(none)>
2016-06-05 18:28:59 +0800

    Output

--- Output/output.c
+++ Output/output.c
@@ -5,27 +5,45 @@
 #include <pcre2posix.h>
 
 
-
-static int num = 0;
-void output( char *host,char *body,int type )
-{
-	if ( type != 3 )
-		return;
-	char filename[100] = { 0 };
-	int fd;
-	num++;
-	sprintf( filename, "file%d%s%d",num,host,type);
-	fd = open( filename , O_CREAT | O_WRONLY ,0666);
-	if ( fd < 0 )
-		return;
-	write( fd , body , strlen( body ));
-	close(fd);
-	
-}
-
 /*
+   static int num = 0;
+   void output( char *host,char *body,int type )
+   {
+   if ( type != 3 )
+   return;
+   char filename[100] = { 0 };
+   int fd;
+   num++;
+   sprintf( filename, "file%d%s%d",num,host,type);
+   fd = open( filename , O_CREAT | O_WRONLY ,0666);
+   if ( fd < 0 )
+   return;
+   write( fd , body , strlen( body ));
+   close(fd);
+
+   }
+ */
+
 void output( char *host , char *body , int type)
 {
-		
+	char *regexstr = "<a.*?href=(\\\"|\').*?(\\\"|\").*?>.*?</a>";
+	regex_t reg;
+	regmatch_t pmatch;
+
+	if (regcomp(&reg,regexstr,0)!=0)
+	{
+		puts("comp err");
+		return;
+	}
+
+	if (regexec(&reg,body,1,&pmatch,0 ) == REG_NOMATCH)
+	{
+		puts("no match");
+	}
+	else
+	{
+
+	}
+	regfree(&reg);
 }
-*/
+
